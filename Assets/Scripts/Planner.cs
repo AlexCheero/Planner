cc3aacd86b24be98a0437b9dcedfa8ebf3c9c281
alexcheero <alexcheero157@gmail.com>
2015-04-29 20:41:06 +0400

    coroutine planner

--- Assets/Scripts/Planner.cs
+++ Assets/Scripts/Planner.cs
@@ -1,11 +1,23 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace GOAP
 {
-    public class Planner
+    public class Planner : MonoBehaviour
     {
-        //todo try use multithreading/coroutines to increase speed
-        private void PlanActions(WorldModel model, int maxDepth, out int[] actions, out WorldModel[] models)
+        public delegate void PlanningFinishedCallback(WorldModelNode node);
+
+        private int _workingPlanRoutines;
+        private float _bestDiscontentment;
+        private WorldModelNode _bestNode;
+        private PlanningFinishedCallback _callback;
+
+        public void Subscribe(PlanningFinishedCallback callback)
+        {
+            _callback = callback;
+        }
+
+        public void PlanActions(WorldModel model, int maxDepth, out int[] actions, out WorldModel[] models)
         {
             var modelsSequence = new WorldModel[maxDepth + 1];
             var actionSequence = new int[maxDepth];
@@ -52,6 +64,41 @@ namespace GOAP
 
             actions = bestActionSequence;
             models = bestModelsSequence;
+        }
+
+        public IEnumerator CoroutinePlanActions(WorldModel model, int maxDepth)
+        {
+            _workingPlanRoutines = 1;
+            _bestDiscontentment = Mathf.Infinity;
+            StartCoroutine(PlanWorldActions(model, maxDepth, 0, new WorldModelNode(model)));
+
+            while (_workingPlanRoutines > 0)
+                yield return new WaitForEndOfFrame();
+
+            _callback(_bestNode);
+        }
+        
+        private IEnumerator PlanWorldActions(WorldModel model, int maxDepth, int currentDepth, WorldModelNode parentNode)
+        {
+            if (currentDepth >= maxDepth)
+            {
+                if (model.Discontentment < _bestDiscontentment)
+                {
+                    _bestDiscontentment = model.Discontentment;
+                    _bestNode = parentNode;
+                }
+
+                yield return null;
+            }
+            foreach (var action in model.ActionsMembership)
+            {
+                var node = new WorldModelNode(model, action.First.BoardIndex, parentNode);
+                var newModel = new WorldModel(model);
+                StartCoroutine(PlanWorldActions(newModel, maxDepth, currentDepth + 1, node));
+            }
+
+            _workingPlanRoutines--;
+            yield return null;
         }
     }
 }
---
+++ Assets/Scripts/WorldModelNode.cs
@@ -0,0 +1,31 @@
+﻿namespace GOAP
+{
+    public class WorldModelNode
+    {
+        public int[] ActionIndexes;
+        public WorldModel[] Models;
+
+        public WorldModelNode(WorldModel model)
+        {
+            ActionIndexes = new int[0];
+            Models = new[] { model };
+        }
+
+        public WorldModelNode(WorldModel model, int actionIndex, WorldModelNode parentNode)
+        {
+            ActionIndexes = MakeExtendedArray(parentNode.ActionIndexes, actionIndex);
+            Models = MakeExtendedArray(parentNode.Models, model);
+        }
+
+        private T[] MakeExtendedArray<T>(T[] array, T lastValue)
+        {
+            var length = array.Length;
+            var extendedArray = new T[length + 1];
+            for (var i = 0; i < length; i++)
+                extendedArray[i] = array[i];
+            extendedArray[length] = lastValue;
+
+            return extendedArray;
+        }
+    }
+}
