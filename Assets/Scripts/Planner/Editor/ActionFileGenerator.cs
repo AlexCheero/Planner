da08ed5ae915ac3867ee2b149e38a578f745fc18
alexcheero <alexcheero157@gmail.com>
2016-02-24 16:20:43 +0300

    generating right data, dirty version

--- Assets/Scripts/Planner/Editor/ActionFileGenerator.cs
+++ Assets/Scripts/Planner/Editor/ActionFileGenerator.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using UnityEditor;
+using UnityEngine;
 
 namespace GOAPEditor
 {
@@ -17,6 +18,7 @@ namespace GOAPEditor
         private const string TemplatesPath = ActionStuffPath + @"Templates\";
         private const string ActionFactoryTemplate = "ActionFactoryTemplate";
         private const string PlannerActionTemplate = "PlannerActionTemplate";
+        private const string ActionType = "EActionType Type";
         private const string FactoryTemplatePath = TemplatesPath + ActionFactoryTemplate + ".cs";
         private const string ActionTemplatePath = TemplatesPath + PlannerActionTemplate + ".cs";
         private const string ActionPostfix = "PlannerAction";
@@ -31,48 +33,72 @@ namespace GOAPEditor
             _entriesToDelete = entriesToDelete;
 
             DeleteFiles();
-            GenerateEnum();
-            RewriteActionBoard();
+
+            var datas = new List<FileGenData> { GenerateEnum(), RewriteActionBoard() };
             foreach (var entry in _entriesList)
             {
-                GenerateFactory(entry);
-                GenerateAction(entry);
+                var factory = GenerateFactory(entry);
+                if (factory.FilePath != null && factory.Content != null)
+                    datas.Add(factory);
+
+                var action = GenerateAction(entry);
+                if (action.FilePath != null && action.Content != null)
+                {
+                    action.Content = RewriteContent(action.Content, ActionType, new List<string> { "get { return EActionType." + entry + "; }" }, "", 3);
+                    datas.Add(action);
+                }
             }
+
+            foreach (var data in datas)
+                WriteFile(data);
         }
 
-        private void GenerateEnum()
+        private FileGenData GenerateEnum()
         {
-            RewriteFile(EnumFilePath, "EActionType");
+            var fileContent = File.ReadAllText(EnumFilePath);
+            var content = RewriteContent(fileContent, "enum EActionType", _entriesList);
+
+            return new FileGenData
+            {
+                Content = content,
+                FilePath = EnumFilePath
+            };
         }
 
-        private void RewriteActionBoard()
+        private FileGenData RewriteActionBoard()
         {
-            RewriteFile(ActionBoardPath, "_factories = new List<IActionFactory>", "ActionFactory.Instance", 3);
+            var fileContent = File.ReadAllText(ActionBoardPath);
+            var content = RewriteContent(fileContent, "_factories = new List<IActionFactory>", _entriesList, "ActionFactory.Instance", 3);
+
+            return new FileGenData
+            {
+                Content = content,
+                FilePath = ActionBoardPath
+            };
         }
 
-        private void RewriteFile(string path, string tag, string append = "", int indentCount = 2)
+        private string RewriteContent(string fileContent, string tag, List<string> toRewrite, string append = "", int indentCount = 2)
         {
-            var fileContent = File.ReadAllText(path);
-
             var outerIndent = "";
             var innerIndent = "";
             MakeIndents(indentCount, ref outerIndent, ref innerIndent);
 
-            var newFactories = new StringBuilder(tag + "\n" + outerIndent + "{\n");
+            var newItems = new StringBuilder(tag + "\n" + outerIndent + "{\n");
 
-            for (int i = 0; i < _entriesList.Count; i++)
+            for (int i = 0; i < toRewrite.Count; i++)
             {
-                var factory = _entriesList[i] + append;
-                if (i < _entriesList.Count - 1)
-                    factory += ",\n";
+                var item = toRewrite[i] + append;
+                if (i < toRewrite.Count - 1)
+                    item += ",\n";
                 else
-                    factory += "\n";
-                newFactories.Append(innerIndent + factory);
+                    item += "\n";
+                newItems.Append(innerIndent + item);
             }
-            newFactories.Append(outerIndent + "}");
+            var pattern = newItems.ToString().Contains("}") ? tag + @"\s*{\s*get { throw new System.NotImplementedException\(\); }\s*}" : tag + @"\s*{(.|\n)*?}";
+            newItems.Append(outerIndent + "}");
 
-            var pattern = tag + @"\s*{(.|\n)*?}";
-            WriteFile(pattern, path, fileContent, newFactories.ToString());
+            var regex = new Regex(pattern);
+            return regex.Replace(fileContent, newItems.ToString());
         }
 
         private void MakeIndents(int indentCount, ref string outerIndent, ref string innerIndent)
@@ -92,32 +118,50 @@ namespace GOAPEditor
             innerIndent += "\t";
         }
 
-        private void GenerateFactory(string action)
+        private FileGenData GenerateFactory(string action)
         {
-            GenerateNewFile(action + FactoryPostfix, FactoriesFolderPath, FactoryTemplatePath, ActionFactoryTemplate);
+            return GenerateNewContent(action + FactoryPostfix, FactoriesFolderPath, FactoryTemplatePath, ActionFactoryTemplate);
         }
 
-        private void GenerateAction(string action)
+        private FileGenData GenerateAction(string action)
         {
-            GenerateNewFile(action + ActionPostfix, GeneratedActionsFolderPath, ActionTemplatePath, PlannerActionTemplate);
+            return GenerateNewContent(action + ActionPostfix, GeneratedActionsFolderPath, ActionTemplatePath, PlannerActionTemplate);
         }
 
-        private void GenerateNewFile(string actionWithPostfix, string targetFolderPath, string templatePath, string pattern)
+        private FileGenData GenerateNewContent(string actionWithPostfix, string targetFolderPath, string templatePath, string pattern)
         {
             var fileName = actionWithPostfix;
             var fullPathWithExtension = targetFolderPath + fileName + ".cs";
             if (File.Exists(fullPathWithExtension))
-                return;
+                return new FileGenData();
 
             var templateContent = File.ReadAllText(templatePath);
-            WriteFile(pattern, fullPathWithExtension, templateContent, fileName);
+            
+            var regex = new Regex(pattern);
+            var newContent = regex.Replace(templateContent, fileName);
+            return new FileGenData
+            {
+                FilePath = fullPathWithExtension,
+                Content = newContent
+            };
         }
 
-        private void WriteFile(string pattern, string filePath, string templateContent, string replacement)
+        private void WriteFile(string filePath, string newContent)
         {
-            var regex = new Regex(pattern);
-            File.WriteAllText(filePath, regex.Replace(templateContent, replacement));
+            if (string.IsNullOrEmpty(filePath) || string.IsNullOrEmpty(newContent))
+                return;
+
+            File.WriteAllText(filePath, newContent);
             AssetDatabase.ImportAsset(filePath);
+        }
+
+        private void WriteFile(FileGenData data)
+        {
+            if (string.IsNullOrEmpty(data.FilePath) || string.IsNullOrEmpty(data.Content))
+                return;
+
+            File.WriteAllText(data.FilePath, data.Content);
+            AssetDatabase.ImportAsset(data.FilePath);
         }
 
         private void DeleteFiles()
@@ -139,10 +183,8 @@ namespace GOAPEditor
 
         private struct FileGenData
         {
-            public string Pattern;
             public string FilePath;
-            public string TemplateContent;
-            public string Replacement;
+            public string Content;
         }
     }
 }
